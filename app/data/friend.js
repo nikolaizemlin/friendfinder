0e3996ee79d40704a54504e441410903a60f8922
Nick Hosley <nhosley1976@gmail.com>
2018-07-31 07:13:16 -0700

    added new files for friend finder

---
+++ app/data/friend.js
@@ -0,0 +1,38 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Nick Hosley',
+    profilePic: 'https://nikolaizemlin.github.io/Portfolio/img/about.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "1",
+      "3",
+      "4"
+    ]
+  },
+  {
+    name: 'Elmer Fudd',
+    profilePic: 'https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/elmer-fudd-looney-tunes-webtoons-19.4.jpg',
+    scores: [
+      "2",
+      "2",
+      "3",
+      "2",
+      "2",
+      "1",
+      "2",
+      "2",
+      "2",
+      "2"
+    ]
+  }
+]
+
+module.exports = friendList;
